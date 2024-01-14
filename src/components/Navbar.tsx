5ff00d37220993f154c248e07bd49f1e65250a3e
Christianto Ega Prasetyo <dionisius.ega26@gmail.com>
2024-01-14 22:20:12 +0700

    add components Notifications, Navbar, Slider, Featured, and Footer

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import React from 'react'
+import Menu from './Menu'
+import CartIcon from './CartIcon';
+import Image from 'next/image';
+
+const Navbar = () => {
+  const user = false;
+
+  return (
+    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
+      <div className='hidden md:flex gap-4 md:flex-1'>
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      <div className='text-xl md:font-bold md:flex-1 text-center'>
+        <Link href="/">
+          PIZZATARIA
+        </Link>
+      </div>
+      <div className='md:hidden'>
+        <Menu />
+      </div>
+      <div className='hidden md:flex gap-4 items-center justify-end md:flex-1'>
+        <div className='md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+          <Image src="/phone.png" alt='' width={20} height={20} />
+          <span>1233 2768 444</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
+    </div >
+  )
+}
+
+export default Navbar
